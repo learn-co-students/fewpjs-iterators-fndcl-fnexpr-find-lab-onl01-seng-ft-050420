839aab187a3576f867bbc819379314aba21cf0b3
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-12 16:12:43 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let win = array.find(function(game){
+    return game.result ===  "W"
+  })
+  if (!!win){
+    return win.year
+  }
+}

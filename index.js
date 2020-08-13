effb2a4e60a8fb9d07b6d70684a47894ab71f72a
qu1xs <kevinnealmurphy@gmail.com>
2020-08-12 22:27:05 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  const win = array.find(object => object.result === "W");
+  return !!win ? win.year : undefined;
+}

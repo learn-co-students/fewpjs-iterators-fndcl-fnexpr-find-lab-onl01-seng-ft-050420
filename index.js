dd54553a5f5ebe23e6a2a1b045d6ded6eb6e35b0
leobr <leobrionesherrera@gmail.com>
2020-08-14 13:35:13 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let winner = arr.find(function(elem) { return elem.result === "W" });
+  if (winner) return winner.year;
+}

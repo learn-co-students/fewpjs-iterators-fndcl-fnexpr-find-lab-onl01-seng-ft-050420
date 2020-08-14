d469c76f8ba443a9baee1994a910e4cc574112fa
uharston <uharston@gmail.com>
2020-08-14 11:20:56 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+const superBowlWin = (arr) => {
+ let result = arr.find( game => { return game.result === "W" })
+ return !!result ? result.year : undefined
+ 
+}

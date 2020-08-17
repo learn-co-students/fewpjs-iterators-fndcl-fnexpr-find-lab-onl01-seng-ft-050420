336f81f99490ca843d9dde73ca868f2f3c31876b
gd-m <gitanjalidmule@gmail.com>
2020-08-17 11:21:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+superbowlWin = (array) => {
+  let result = array.find((obj) => {
+    return obj.result === "W"
+  })
+  return !!result ? result.year : undefined;
 }
+
+

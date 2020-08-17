79df6424832c4d3cec0d99811611a2cd202589a3
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-17 19:40:15 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let winner = record.find(function(s) {
+    //let result = s.result === "W"
+    //return result
+    return s.result === "W"
+  })
+  if (winner) {
+    return winner.year
+  }
+}

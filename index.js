63a2f924c98a4865669808c496613021854730a3
anette87 <anettem.rivera@gmail.com>
2020-08-17 20:18:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  let win = records.find( function(record) { return record.result === 'W' });
+  if (win) {
+    return win.year;
+  }else {
+    return undefined;
+  }
 }
+   
+
+
